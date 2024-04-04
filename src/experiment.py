17f8ec7254dc64371a0d8b14d2e0ec58c10bb748
Adam <acharane@unibz.it>
2024-04-04 04:20:47 +0200

    Rename module folder clustering -> selection

--- src/experiment.py
+++ src/experiment.py
@@ -1,22 +1,26 @@
 from data import Data
-from clustering import LIST_OF_ALGORITHMS, ClustersInfo
+from selection import LIST_OF_ALGORITHMS, ClustersInfo
 
 
 class Experiment:
-    def __init__(self, metadata):
-
-        self.description = metadata["description"]
-
-        data = Data(**metadata["data"])
-
-        algorithm_name = next(iter(metadata["algorithm"].keys()))
-        algorithm_params = metadata["algorithm"][algorithm_name]
-        algorithm = LIST_OF_ALGORITHMS[algorithm_name](**algorithm_params)
-
-        self.cluster_info = ClustersInfo(data, algorithm)
+    def __init__(self, description, data, algorithm) -> None:
+        self.description = description
+        self.selection_method = ClustersInfo(data, algorithm)
 
     def run(self):
-        return self.cluster_info.get_info_df()
+        return self.selection_method.get_info_df()
 
     def __repr__(self) -> str:
         return self.description
+
+
+def experiment_parser(metadata) -> Experiment:
+    description = metadata["description"]
+
+    data = Data(**metadata["data"])
+
+    algorithm_name = next(iter(metadata["algorithm"].keys()))
+    algorithm_params = metadata["algorithm"][algorithm_name]
+    algorithm = LIST_OF_ALGORITHMS[algorithm_name](**algorithm_params)
+
+    return Experiment(description, data, algorithm)
