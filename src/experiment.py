d89c39f329364a7fe66a5d8dc47d6565da64de60
acharane <adamcharane@gmail.com>
2024-04-10 16:04:20 +0200

    Classification and evaluation starter code

--- src/experiment.py
+++ src/experiment.py
@@ -1,19 +1,34 @@
 from data import Data
 from selection import LIST_OF_ALGORITHMS, ClustersInfo
 from selection.methods import ClusterBased
+from sklearn.linear_model import LogisticRegression
 
 
 class Experiment:
+    # TODO: Parameters have to come from some experiment metadata class!
     def __init__(self, description, data, algorithm) -> None:
         self.description = description
-        self.selection_method = ClustersInfo(data, algorithm)
+        self.data: Data = data
+        self.selection_method = ClustersInfo(self.data, algorithm)
+        # TODO: Initialize the selection mathod and selection somewhere else
         self.selection = ClusterBased(self.selection_method)
+        # TODO: Initialize classifier somewhere else and pass it to this class
+        self.classifier = LogisticRegression(random_state=0)
 
     def info_df(self):
         return self.selection_method.info()
 
     def get_shapelets(self):
-        self.selection.select()
+        return self.selection.select()
+
+    def classify(self):
+        shapelets = self.get_shapelets()
+        train = self.data.shapelet_transform(shapelets)
+        test = self.data.shapelet_transform(shapelets, train=False)
+        self.classifier.fit(train, self.data._train.y)
+        preds = self.classifier.predict(test)
+        # TODO: Separate the evaluation 
+        return sum(preds == self.data._test.y) / len(preds)
 
     def __repr__(self) -> str:
         return self.description
