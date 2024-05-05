86b7828ea6c6056f9cc7c99e5609041abf2ac16b
Adam Charane <adamcharane@gmail.com>
2024-05-05 17:38:58 +0200

    Move experiments methods to experiments

--- src/experiments/data.py
+++ src/experiments/data.py
@@ -1,16 +1,13 @@
-import os
-from pathlib import Path
 import numpy as np
 from aeon.datasets import load_from_tsfile
-from sklearn.metrics import accuracy_score
+
+from .config import DATA_PATH
 
 
 def get_dataset(
     dataset_name,
     train,
 ) -> tuple[np.ndarray, np.ndarray]:
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    DATA_PATH = Path(BASE_DIR).parent / "data"
     split = "TRAIN" if train else "TEST"
     filepath = str(DATA_PATH / f"{dataset_name}/{dataset_name}_{split}.ts")
     x, y = load_from_tsfile(filepath)
@@ -25,24 +22,3 @@ class Data:
         self.dataset_name = dataset_name
         self.X_train, self.y_train = get_dataset(dataset_name, train=True)
         self.X_test, self.y_test = get_dataset(dataset_name, train=False)
-
-
-def transform(data, Approach, params={}):
-    X_train = data.X_train
-    y_train = data.y_train
-    X_test = data.X_test
-    y_test = data.y_test
-    app = Approach(**params)
-    app.fit(X_train, y_train)
-
-    X_tr = app.transform(X_train)
-    X_te = app.transform(X_test)
-    return X_tr, y_train, X_te, y_test
-
-
-def evaluate(X_tr, y_tr, X_te, y_te, Model, params={}):
-    model = Model(**params)
-    model.fit(X_tr, y_tr)
-    y_pred = model.predict(X_te)
-    acc = accuracy_score(y_pred, y_te)
-    return acc
