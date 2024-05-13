fb915e43c23b9cc31f4681e1bfdc903ec1aa215f
Adam Charane <adamcharane@gmail.com>
2024-05-13 10:58:59 +0200

    Add some exception classes

---
+++ src/helpers/exceptions.py
@@ -0,0 +1,18 @@
+class DatasetUnreadable(Exception):
+    pass
+
+
+class TransformationDoesNotExist(Exception):
+    pass
+
+
+class TransformationFailrue(Exception):
+    pass
+
+
+class ClassificationDoesNotExist(Exception):
+    pass
+
+
+class ClassificationFailure(Exception):
+    pass
