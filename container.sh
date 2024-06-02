ed23e8cb7838421557fb2c64fb357723ec7fdf6f
Adam Charane <adamcharane@gmail.com>
2024-06-02 22:58:30 +0200

    add functions and execute them instead of running stuff sequentially

--- container.sh
+++ container.sh
@@ -2,17 +2,28 @@ VOLUME_NAME=shapelets_pgdata
 IMAGE_NAME=shapelets_image
 CONTAINER_NAME=shapelets
 
-podman volume exists $VOLUME_NAME
-if [[ $? -ne 0 ]]; then
-  podman volume create $VOLUME_NAME
-fi
 
-podman image exists $IMAGE_NAME
-if [[ $? -ne 0 ]]; then
+create_volume_if_not_exist(){
+  podman volume exists $VOLUME_NAME
+  if [[ $? -ne 0 ]]; then
+    podman volume create $VOLUME_NAME
+  fi
+}
+
+build_image(){
+  create_volume_if_not_exist
   podman build -t $IMAGE_NAME ./
-fi
+}
+
+build_image_if_not_exist(){
+  podman image exists $IMAGE_NAME
+  if [[ $? -ne 0 ]]; then
+    build_image
+  fi
+}
 
 run_container(){
+  build_image_if_not_exist
   podman run --replace --rm \
     --name $CONTAINER_NAME \
     -p 5432:5432 -p 8888:8888 -p 8501:8501 \
@@ -31,19 +42,20 @@ if [[ $# -ne 0 ]]; then
   if [[ $1 == "start" ]]; then
     run_container_if_off
   elif [[ $1 == "stop" ]]; then
-    if [[ $(podman ps | grep $CONTAINER_NAME | wc -l) -eq 1 ]]; then
-      podman container stop shapelets
-    fi
+      podman container stop $CONTAINER_NAME
   elif [[ $1 == "build" ]]; then
-    podman build -t $IMAGE_NAME ./
+    build_image
     run_container
   elif [[ $1 == "bash" ]]; then
     run_container_if_off
-    podman exec -it shapelets bash
+    podman exec -it $CONTAINER_NAME bash
+  elif [[ $1 == "psql" ]]; then
+    run_container_if_off
+    podman exec -it $CONTAINER_NAME psql -U postgres -d shapelets
   elif [[ $1 == "run" ]]; then
     run_container_if_off
-    podman exec shapelets bash /scripts/run.sh $2
+    podman exec $CONTAINER_NAME bash /scripts/run.sh $2
   else
-    echo "Wrong argument, run using: ./container [build|bash|run]]"
+    echo "Wrong argument, run using: ./container [start|stop|build|bash|psql|run]]"
   fi
 fi
