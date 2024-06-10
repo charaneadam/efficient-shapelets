b10d8341271dd07062c094b1c0d2e32bdcf85aaa
Adam Charane <adamcharane@gmail.com>
2024-06-11 00:53:27 +0200

    Measure time for fitting transforming and classifying as well as f1 score

--- src/main.py
+++ src/main.py
@@ -1,41 +1,66 @@
 from time import perf_counter
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 from src.methods import SelectionMethod
 from src.classifiers import CLASSIFIERS, Classifier
 from .data import Data
 
 
-def _transform(data, method_name, method_params={}):
-    X_train = data.X_train
-    y_train = data.y_train
-    X_test = data.X_test
-    y_test = data.y_test
+def _transform(method, X_train, X_test):
+    X_tr = method.transform(X_train)
+    X_te = method.transform(X_test)
+    return X_tr, X_te
+
+
+def _fit(method_name, method_params, X_train, y_train):
     method = SelectionMethod(method_name)
     method.set_parameters(method_params)
     method.fit(X_train, y_train)
-
-    X_tr = method.transform(X_train)
-    X_te = method.transform(X_test)
-    return X_tr, y_train, X_te, y_test
+    return method
 
 
 def _classify(X_tr, y_tr, X_te, y_te, model_name, info):
     model = Classifier(model_name)
+
+    start = perf_counter()
     model.fit(X_tr, y_tr)
+    end = perf_counter()
+    fit_time = end - start
+
+    start = perf_counter()
     y_pred = model.predict(X_te)
+    end = perf_counter()
+    predict_time = end - start
+
     acc = accuracy_score(y_pred, y_te)
-    info["models"][model_name] = acc
+    if len(set(y_te)) > 2:
+        f1 = f1_score(y_pred, y_te, average="weighted")
+    else:
+        f1 = f1_score(y_pred, y_te)
+
+    info["models"][model_name] = {
+        "fit_time": fit_time,
+        "predict_time": predict_time,
+        "accuracy": acc,
+        "f1": f1,
+    }
 
 
 def transform_dataset(data: Data, method_name, params, info):
     start = perf_counter()
-    X_tr, y_train, X_te, y_test = _transform(data, method_name, params)
+    method = _fit(method_name, params, data.X_train, data.y_train)
     end = perf_counter()
-    elapsed_time = end - start
+    fit_time = end - start
+
+    start = perf_counter()
+    X_tr, X_te = _transform(method, data.X_train, data.X_test)
+    end = perf_counter()
+    transform_time = end - start
+
     num_shapelets = X_tr.shape[1]
-    info["time"] = elapsed_time
+    info["fit_time"] = fit_time
+    info["transform_time"] = transform_time
     info["n_shapelets"] = num_shapelets
-    return X_tr, y_train, X_te, y_test
+    return X_tr, data.y_train, X_te, data.y_test
 
 
 def classify_dataset(X_tr, y_train, X_te, y_test, info):
