2e15c7ca23b4aacae9a2be99adf81296179bfcd0
Adam Charane <adamcharane@gmail.com>
2024-06-11 02:56:58 +0200

    define db models

--- src/database.py
+++ src/database.py
@@ -1,6 +1,6 @@
 from peewee import PostgresqlDatabase
 from peewee import Model, ForeignKeyField
-from peewee import CharField, IntegerField, FloatField, DoubleField
+from peewee import CharField, IntegerField, FloatField, TextField, BooleanField
 
 db = PostgresqlDatabase(
     database="shapelets", user="postgres", host="localhost", port=5432
@@ -13,47 +13,40 @@ class BaseModel(Model):
 
 
 class Dataset(BaseModel):
+    name = CharField(unique=True)
     data_type = CharField()
-    name = CharField()
     train = IntegerField()
     test = IntegerField()
     n_classes = IntegerField()
     length = IntegerField()
+    missing_values = BooleanField(default=False)
 
 
-class Method(BaseModel):
-    method_name = CharField()
-    dataset = ForeignKeyField(Dataset, backref="methods")
+class SelectionMethod(BaseModel):
+    name = CharField(unique=True)
+    description = TextField(null=True)
 
 
-class MethodParameter(BaseModel):
-    name = CharField()
-    value = FloatField()
-    runtime = DoubleField()
-    method = ForeignKeyField(Method, backref="parameters")
+class Classifier(BaseModel):
+    name = CharField(unique=True)
+    description = TextField(null=True)
 
 
-class MethodShapelets(BaseModel):
-    length = IntegerField()
-    number = IntegerField()
-    dataset = ForeignKeyField(Dataset, backref="shapelets_properties")
-    method = ForeignKeyField(Method, backref="shapelets_properties")
+class DataTransformation(BaseModel):
+    fit_time = FloatField()
+    transform_time = FloatField()
+    n_shapelets = IntegerField()
+    dataset = ForeignKeyField(Dataset, backref="transformations")
+    method = ForeignKeyField(SelectionMethod, backref="transformations")
 
 
-class ModelClassification(BaseModel):
-    model_name = CharField()
+class Classification(BaseModel):
     accuracy = FloatField()
     f1 = FloatField()
-    method_shapelets = ForeignKeyField(MethodShapelets, backref="classifiers")
-
-
-TABLES = [
-    Dataset,
-    Method,
-    MethodParameter,
-    MethodShapelets,
-    ModelClassification,
-]
+    train_time = FloatField()
+    test_time = FloatField()
+    classifier = ForeignKeyField(Classifier, backref="classifications")
+    data = ForeignKeyField(DataTransformation, backref="classifications")
 
 
 def init_ucr_metadata():
@@ -74,5 +67,35 @@ def init_ucr_metadata():
         Dataset.create(**dict(zip(names, row)))
 
 
+def insert_method_names():
+    # from src.methods import SELECTION_METHODS
+
+    # methods_names = SELECTION_METHODS.keys()
+    methods_names = [
+        "RandomDilatedShapelets",
+        "RandomShapelets",
+        "LearningShapelets",
+        "FastShapeletSelection",
+    ]
+    for method_name in methods_names:
+        SelectionMethod.create(name=method_name)
+
+
+def insert_classifiers_names():
+    from src.classifiers import CLASSIFIERS
+
+    for classifier_name in CLASSIFIERS.keys():
+        Classifier.create(name=classifier_name)
+
+
+def create_tables():
+    TABLES = [SelectionMethod, Classifier, DataTransformation, Classification]
+    db.drop_tables(TABLES)
+    db.create_tables(TABLES)
+    insert_method_names()
+    insert_classifiers_names()
+
+
 if __name__ == "__main__":
     init_ucr_metadata()
+    create_tables()
