5c9c312e40ea874e2d7a32202ba39a68a33c3ba7
Adam Charane <adamcharane@gmail.com>
2024-06-11 03:28:57 +0200

    Set data location in config to point to data inside container

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 BASE_PATH = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-DATA_PATH = BASE_PATH / "data"
+DATA_PATH = Path("/data/")
 RESULTS_PATH = BASE_PATH / "results"
 DATASETS_PATHS = [x[0] for x in os.walk(DATA_PATH)]
 DATASETS_NAMES = list(map(lambda x: x.split("/")[-1], DATASETS_PATHS))
