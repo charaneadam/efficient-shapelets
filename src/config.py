b2dcfed7c39ff38cc9087d53da16960e2c97086f
Adam Charane <adamcharane@gmail.com>
2024-06-11 03:31:34 +0200

    Remove unnecessary configurations

--- src/config.py
+++ src/config.py
@@ -2,10 +2,6 @@ import os
 from pathlib import Path
 
 
-BASE_PATH = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DATA_PATH = Path("/data/")
+BASE_PATH = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 RESULTS_PATH = BASE_PATH / "results"
-DATASETS_PATHS = [x[0] for x in os.walk(DATA_PATH)]
-DATASETS_NAMES = list(map(lambda x: x.split("/")[-1], DATASETS_PATHS))
-DATASETS = dict(zip(DATASETS_NAMES, DATASETS_PATHS))
-METADATA_PATH = DATA_PATH / "metadata.csv"
