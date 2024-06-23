cab717987385c90e5a96aa5cd4568f45999876ec
Adam Charane <adamcharane@gmail.com>
2024-06-23 12:14:18 +0200

    save execution time of values of different shapelet evaluation methods

--- src/benchmarks/windows_evaluation/bruteforce.py
+++ src/benchmarks/windows_evaluation/bruteforce.py
@@ -1,41 +1,60 @@
 from time import perf_counter
 import numpy as np
-from numba import njit, prange
+from numba import njit, prange, objmode
 
 from src.storage.data import Data, Windows
 from .db import save, init_windows_tables
-from src.benchmarks.windows_evaluation.utils import distance_numba
+from src.benchmarks.windows_evaluation.utils import (
+    distance_numba,
+    fstat,
+    info_gain,
+    silhouette,
+)
 
 
-@njit(cache=True, fastmath=True)
+@njit(cache=True, fastmath=True, parallel=True)
 def _eval_silhouette_bruteforce(X, y, windows, windows_per_ts):
     n_windows = windows.shape[0]
     n_ts = X.shape[0]
-    res = np.zeros(n_windows)
+    res = np.zeros((n_windows, 6))# 6: 3 for sil,infogain,fstat, and 3 for time
     for window_id in prange(n_windows):
         window = windows[window_id]
         window_ts_id = window_id // windows_per_ts
         window_label = y[window_ts_id]
-        a, b = 0.0, 0.0
-        a_tot = 0
-        b_tot = 0
-        for ts_id in prange(n_ts):
+        dists_to_ts = np.zeros(n_ts)
+        for ts_id in range(n_ts):
             if window_ts_id == ts_id:
                 continue
             dist = distance_numba(X[ts_id], window)
-            if y[ts_id] == window_label:
-                a += dist
-                a_tot += 1
-            else:
-                b += dist
-                b_tot += 1
-        a /= a_tot
-        b /= b_tot
-        if b > a:
-            mx = b
-        else:
-            mx = a
-        res[window_id] = (b - a) / mx
+            dists_to_ts[ts_id] = dist
+
+        with objmode(start='f8'):
+            start = perf_counter()
+        silhouette_score = silhouette(dists_to_ts, window_label, y, window_ts_id)
+        with objmode(end='f8'):
+            end = perf_counter()
+        silhouette_time = end - start
+        res[window_id][0] = silhouette_score
+        res[window_id][1] = silhouette_time
+
+        with objmode(start='f8'):
+            start = perf_counter()
+        fstat_score = fstat(dists_to_ts, window_label, y, window_ts_id)
+        with objmode(end='f8'):
+            end = perf_counter()
+        fstat_time = end - start
+        res[window_id][2] = fstat_score
+        res[window_id][3] = fstat_time
+
+        with objmode(start='f8'):
+            start = perf_counter()
+        infgain_score = info_gain(dists_to_ts, window_label, y, window_ts_id)
+        with objmode(end='f8'):
+            end = perf_counter()
+        infogain_time = end - start
+        res[window_id][4] = infgain_score
+        res[window_id][5] = infogain_time
+
     return res
 
 
