8518f47a0c42fdf8bf43b06e4a863e9a28a3652a
Adam Charane <adamcharane@gmail.com>
2024-07-04 00:04:09 +0200

    classification using variables length

--- src/benchmarks/classification/variable_lengths.py
+++ src/benchmarks/classification/variable_lengths.py
@@ -1,10 +1,6 @@
 import warnings
-from numba import set_num_threads
-import numpy as np
 import pandas as pd
 
-from src.benchmarks.get_experiment import get_datasets
-from src.benchmarks.windows_evaluation.bruteforce import _eval_bruteforce
 from src.benchmarks.classification.utils import _classify, transform
 
 from src.storage.database import engine
@@ -12,81 +8,28 @@ from sqlalchemy import inspect
 from src.classifiers import CLASSIFIERS_NAMES
 from src.storage.data import Data
 
-
-def sample_subsequence_positions(ts_length):
-    """
-    Given the length of a time series, this function return a candidate s.t:
-    - Starting position is in the range [0, index_of(90% of the ts_length)]
-    - The length is in the range of [5% of ts_length, 70% of ts_length]
-    - We set the length to a max of (3, and length). This is for the case
-        when we take, say 5% of a really small time series. eg. 5% of 12
-        is 0.6, which will be rounded to 0
-    - Ending position is simply: start_pos + length (in case end pos
-        is out of bound, we set it to the last timestamp)
-    """
-    start_pos = np.random.randint(int(0.9 * ts_length))
-    length = np.random.randint(int(0.05 * ts_length), int(0.7 * ts_length))
-    length = max(length, 3)
-    end_pos = min(ts_length, start_pos + length)
-    return start_pos, end_pos
+CLASSIFICATION_VARIABLE_TABLE_NAME = "classification_variable_lengths"
 
 
-def candidates_and_tsids(data):
-    """Given some data, this function samples a number of shapelets with
-    different lengths. It returns the Z-normalized candidates as well as
-    the ids of the time series from which they have been extracted."""
-    ts_length = data.ts_length
-    n_shapelets = max(300, int(0.2 * ts_length))
-    labels = list(set(data.y_train))
-    candidates = []
-    ids = []
-    for label in labels:
-        ts_ids = np.where(data.y_train == label)[0]
-        remaining = n_shapelets
-        while remaining > 0:
-            try:
-                ts_id = np.random.choice(ts_ids)
-                start_pos, end_pos = sample_subsequence_positions(ts_length)
-                candidate = data.X_train[ts_id][start_pos:end_pos]
-                candidate = (candidate - np.mean(candidate)) / np.std(candidate)
-                remaining -= 1
-                candidates.append(candidate)
-                ids.append([ts_id, start_pos, end_pos])
-            except:
-                """Failure sometimes happen when normalizing a candidate due to
-                numeric calculations failure (most of the time the standard
-                deviation is 0. When a failure happens, we simply retry
-                with a new sample, and keep repeating this process till
-                the number of candidates is satisfied"""
-                pass
-    return candidates, np.array(ids)
-
-
-def evaluate(data, windows, windows_ts_ids):
-    """Given data, windows and their corresponding ts_ids from which they have
-    been extracted; this function return a dataframe with the scores,
-    their timings, as well as the label of each window"""
-    results = _eval_bruteforce(data.X_train, data.y_train, windows, windows_ts_ids)
-    cols = ["silhouette", "silhouette_time", "fstat", "fstat_time", "gain", "gain_time"]
-    df = pd.DataFrame(results, columns=cols)
-    windows_labels = data.y_train[windows_ts_ids]
-    df["label"] = windows_labels
-    return df
-
-
-def _select_best_k(df, label, method, K):
-    """Returns the best *K* shapelets for representing time series labelled
-    *label* using some *method* ("silhouette", "gain" or "fstat")"""
+def _select_best_k(df, label, method, K, data):
     view = df[df.label == label]
-    return view.sort_values(by=method, ascending=False).index[:K].values
+    info = view.sort_values(by=method, ascending=False)[
+        ["ts_id", "start", "end"]
+    ].values[:K]
+    shapelets = []
+    for ts_id, start, end in info:
+        cand = data.X_train[ts_id][start:end]
+        cand = (cand - cand.mean()) / cand.std()
+        shapelets.append(cand)
+    return shapelets
 
 
-def classify(df, windows, data, method, k):
+def classify(df, data, method, k):
     shapelets = []
     labels = list(set(data.y_train))
     for label in labels:
-        indices = _select_best_k(df, label, method, k)
-        shapelets.extend([windows[index] for index in indices])
+        candidates = _select_best_k(df, label, method, k, data)
+        shapelets.extend(candidates)
     X_tr, X_te = transform(data, shapelets)
     accuracies = {}
     for clf_name in CLASSIFIERS_NAMES:
@@ -99,17 +42,15 @@ def classify(df, windows, data, method, k):
 def compare(dataset_name):
     warnings.simplefilter("ignore")
     data = Data(dataset_name)
-    candidates, candidates_info = candidates_and_tsids(data)
-    df = evaluate(data, candidates, candidates_info[:, 0])
-    df["dataset"] = dataset_name
-    df["ts_id"] = candidates_info[:, 0]
-    df["start"] = candidates_info[:, 1]
-    df["end"] = candidates_info[:, 2]
-    df.to_sql("variable_length_candidates", engine, if_exists="append", index=False)
     results = []
+    df = pd.read_sql(
+        f"""SELECT * FROM random_lengths_candidates
+        WHERE dataset='{dataset_name}'""",
+        engine,
+    )
     for method in ["silhouette", "gain", "fstat"]:
         for K in [3, 5, 10, 20, 50, 100]:
-            accuracies = classify(df, candidates, data, method, K)
+            accuracies = classify(df, data, method, K)
             models_accuracies = [
                 accuracies.get(clf_name, None) for clf_name in CLASSIFIERS_NAMES
             ]
@@ -119,26 +60,32 @@ def compare(dataset_name):
 
 
 def run():
-    datasets = get_datasets()
+    datasets = pd.read_sql(
+        "SELECT DISTINCT dataset FROM random_lengths_candidates", engine
+    ).values.squeeze()
     columns = ["dataset", "method", "K_shapelets"] + CLASSIFIERS_NAMES
     inspector = inspect(engine)
-    TABLE_NAME = "variable_lengths"
-    if inspector.has_table(TABLE_NAME):
-        current_df = pd.read_sql(TABLE_NAME, engine)
+    if inspector.has_table(CLASSIFICATION_VARIABLE_TABLE_NAME):
+        current_df = pd.read_sql(CLASSIFICATION_VARIABLE_TABLE_NAME, engine)
         computed = set(current_df.dataset.unique())
     else:
         computed = set()
     for dataset in datasets:
-        if dataset.length < 60 or dataset.name in computed:
+        if dataset in computed:
             continue
         try:
-            results = compare(dataset.name)
+            results = compare(dataset)
             df = pd.DataFrame(results, columns=columns)
-            df.to_sql(TABLE_NAME, engine, if_exists="append", index=False)
-        except:
-            print(f"Error happened with dataset {dataset.name}")
+            df.to_sql(
+                CLASSIFICATION_VARIABLE_TABLE_NAME,
+                engine,
+                if_exists="append",
+                index=False,
+            )
+        except Exception as e:
+            print(f"Error during classification of dataset {dataset}.")
+            print(f"Error: {e}")
 
 
 if __name__ == "__main__":
-    set_num_threads(4)
     run()
