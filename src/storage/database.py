1113e4ddcbcfa789668969a0003eccb3186d4ead
Adam Charane <adamcharane@gmail.com>
2024-07-04 11:45:12 +0200

    Chnge tables names

--- src/storage/database.py
+++ src/storage/database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from peewee import PostgresqlDatabase
 from peewee import Model
 
-VARIABLE_LENGTH_CANDIDATES_TABLE_NAME = "random_lengths_candidates"
-VARIABLE_LENGTH_TABLE_NAME = "classification_variable_lengths"
-SAME_LENGTH_CLASSIFICATION_TABLE_NAME = "classification_same_lengths"
 SAME_LENGTH_CANDIDATES_TABLE_NAME = "same_length_candidates"
+VARIABLE_LENGTH_CANDIDATES_TABLE_NAME = "variable_length_candidates"
+
+SAME_LENGTH_CLASSIFICATION_TABLE_NAME = "fixed_length_classification"
+VARIABLE_CLASSIFICATION_LENGTH_TABLE_NAME = "variable_length_classification"
 
 db_peewee = PostgresqlDatabase(
     database="shapelets", user="postgres", host="localhost", port=5432
