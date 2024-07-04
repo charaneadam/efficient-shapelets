4dc1488ccb7e8e839877920f6f198b475eb98cf0
Adam Charane <adamcharane@gmail.com>
2024-07-04 17:02:55 +0200

    Set number of cores for classifiers

--- src/classifiers.py
+++ src/classifiers.py
@@ -5,6 +5,8 @@ from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 
+from src.config import NUM_CORES_FOR_CLASSIFICATION
+
 
 CLASSIFIERS = {
     "Logistic Regression": LogisticRegression,
@@ -33,7 +35,7 @@ class Classifier:
     def __init__(self, classifier_name):
         self._alg = CLASSIFIERS[classifier_name]
         params = DEFAULT_CLASSIFIERS_PARAMS.get(classifier_name, {})
-        self._alg = self._alg(**params)
+        self._alg = self._alg(n_jobs=NUM_CORES_FOR_CLASSIFICATION, **params)
 
     def fit(self, X, y):
         self._alg.fit(X, y)
--- src/config.py
+++ src/config.py
@@ -8,17 +8,4 @@ RESULTS_PATH = BASE_PATH / "results"
 
 
 NUM_THREADS = 8
-
-
-LOCAL_ENV = os.environ.get("local", False)
-if LOCAL_ENV:
-    print("Good")
-    SAME_LENGTH_CANDIDATES_TABLE_NAME = os.environ["local"]
-    VARIABLE_LENGTH_CANDIDATES_TABLE_NAME = "variable_length_candidates"
-
-    SAME_LENGTH_CLASSIFICATION_TABLE_NAME = "fixed_lengths"
-    VARIABLE_CLASSIFICATION_LENGTH_TABLE_NAME = "variable_lengths"
-
-
-if __name__ == "__main__":
-    print(LOCAL_ENV)
+NUM_CORES_FOR_CLASSIFICATION = 4
