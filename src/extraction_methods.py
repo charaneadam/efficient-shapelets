f1aaa2ef9307041c5d90258332e03553b67a3580
Adam Charane <adamcharane@gmail.com>
2024-08-04 16:38:42 +0200

    Add clustering approach

--- src/extraction_methods.py
+++ src/extraction_methods.py
@@ -1,4 +1,7 @@
 import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
+from sklearn.preprocessing import StandardScaler
+import faiss
 
 from src.exceptions import NormalizationFailure
 from src.storage.data import Data
@@ -110,68 +113,57 @@ class FSS:
             positions, candidates = fss.transform(X_train[ts_ids_by_label])
             for i in range(len(positions)):
                 candidates[i] = normalize(candidates[i])
+                # remap ts_idx to ts_id (positions[i][0])
                 positions[i][0] = mapper[positions[i][0]]
             self.candidates[label] = candidates
             self.candidates_positions[label] = positions
 
 
 class Centroids:
-    def __init__(self, dataset_info) -> None:
-        self.data_info = dataset_info
+    def __init__(self, data) -> None:
+        self.data: Data = data
+        self.candidates = dict()
+        self.candidates_positions = dict()
 
     def generate_candidates(self):
-        pass
+        # for label in self.data.labels:
+        for label in [1,2,3]:
+            label_positions = np.where(self.data.y_train == label)[0]
+            data_label_view = self.data.X_train[label_positions]
 
+            self.candidates_positions[label] = []
+            self.candidates[label] = []
+            length_percentages = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5]
+            for length in map(
+                lambda x: int(x * self.data.ts_length), length_percentages
+            ):
+                data_label_windows = sliding_window_view(
+                    data_label_view, (1, length)
+                ).squeeze()
+                n_ts, n_windows_per_ts, _ = data_label_windows.shape
+                assert n_ts == sum(self.data.y_train == label)
 
-EXTRACTION_METHODS = {
-    0: {
-        "class": FixedLength,
-        "params": {"window_length": 0.05},
-        "name": "Fixed length 5%",
-    },
-    1: {
-        "class": FixedLength,
-        "params": {"window_length": 0.1},
-        "name": "Fixed length 10%",
-    },
-    2: {
-        "class": FixedLength,
-        "params": {"window_length": 0.2},
-        "name": "Fixed length 20%",
-    },
-    3: {
-        "class": FixedLength,
-        "params": {"window_length": 0.3},
-        "name": "Fixed length 30%",
-    },
-    4: {
-        "class": FixedLength,
-        "params": {"window_length": 0.4},
-        "name": "Fixed length 40%",
-    },
-    5: {
-        "class": FixedLength,
-        "params": {"window_length": 0.5},
-        "name": "Fixed length 50%",
-    },
-    6: {"class": VariableLength, "params": {}, "name": "Random variable length"},
-    7: {"class": FSS, "params": {}, "name": "Fast Shapelet Transform"},
-    # 8: {"class": Centroids, "params": {"number_centroids": 0.1}, "name": "Clustering"},
-}
+                n_total_windows = n_ts * n_windows_per_ts
+                n_centroids = int(np.sqrt(n_total_windows))
+                windows_view = data_label_windows.reshape(n_total_windows, length)
+                windows_view = StandardScaler().fit_transform(windows_view.T).T
 
+                km = faiss.Kmeans(length, n_centroids)
+                km.train(windows_view)
+                dists, indices = km.index.search(windows_view, 1)
+                indices = indices.reshape(-1)
+                dists = dists.reshape(-1)
 
-class ExtractionMethod:
-    def __init__(self, data, method_id) -> None:
-        self.id = method_id
-        method = EXTRACTION_METHODS[method_id]
-        self.method = method["class"](data, **method["params"])
-
-    def extract(self):
-        self.method.generate_candidates()
-
-    def candidates(self):
-        return self.method.candidates
-
-    def candidates_positions(self):
-        return self.method.candidates_positions
-
+                for centroid_index in range(n_centroids):
+                    centroid_windows = np.where(indices == centroid_index)[0]
+                    index_window_minimal_distance = centroid_windows[
+                        np.argmin(dists[centroid_windows])
+                    ]
+                    ts_idx = index_window_minimal_distance // n_windows_per_ts
+                    ts_id = label_positions[ts_idx]
+                    start = index_window_minimal_distance % n_windows_per_ts
+                    end = start + length
+                    self.candidates_positions[label].append([ts_id, start, end])
+                    self.candidates[label].append(
+                        windows_view[index_window_minimal_distance]
+                    )
