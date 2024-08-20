4c6c3c9b2279919f5b5e2faf5ffd5fa29c64b804
Adam Charane <adamcharane@gmail.com>
2024-08-20 11:00:55 +0200

    Update plots script

--- plots.py
+++ plots.py
@@ -1,92 +1,80 @@
-import os
-import duckdb
-import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-cm = 1 / 2.54
-
-remaps = {
-    "evaluation": {0:"silhouette", 1:"F-statistic", 2:"binary info", 3:"information gain", 'multiclass info':"information gain"},
-    "extraction": {6: "random samples", 7: "FSS", 8:"clustering"},
-    "model": {0: "Logistic Regression", 3: "Gaussian Process", 4: "Decision Tree", 5: "Random Forest"}
-}
 
 # Figure configs
-evaluation_colors = {"silhouette": "green", "fstat": "orange", "information gain": "blue"}
-evaluation_order = ["information gain", "silhouette", "fstat"]
-extraction_colors = {"clustering": "green", "FSS": "orange", "random samples": "blue"}
-extraction_order = ["random samples", "clustering", "FSS"]
+cm = 1 / 2.54
+evaluation_colors = {
+    "silhouette": "#0072B2",
+    "fstat": "#E69F00",
+    "information gain": "#009E73",
+}
+evaluation_order = ["silhouette", "information gain", "fstat"]
+extraction_colors = {"clustering": "#0072B2", "FSS": "#D55E00"}
+extraction_order = ["clustering", "FSS"]
 
-db = duckdb.connect()
-db.sql("CREATE VIEW candidates AS select * FROM 'db/candidates.csv'");
-db.sql("CREATE VIEW dataset AS select * FROM 'db/dataset.csv'");
-db.sql("CREATE VIEW classification AS select * FROM 'db/classification.csv'");
-db.sql("CREATE VIEW num_candidates AS SELECT dataset, method as extraction, count(*) as n_candidates from candidates group by all")
 
-db.sql("""
-    CREATE VIEW valid_datasets AS
-    WITH
-        counts AS (
-            SELECT dataset, count(distinct extraction) as n_extraction 
-            FROM classification 
-            WHERE evaluation = 'silhouette' 
-            GROUP BY ALL
-        )
-    SELECT dataset
-    FROM counts
-    WHERE n_extraction = 3 -- keep datasets where all three methods have been tested
-    """)
+# Extraction time
+fig, ax = plt.subplots(figsize=(14 * cm, 6 * cm))
+sns.scatterplot(
+    pd.read_parquet("results/data/extraction.parquet.gzip"),
+    x="numcandidates",
+    y="time",
+    hue="extraction",
+    ax=ax,
+    palette=extraction_colors,
+)
+ax.set_xlabel("Number of possible subsequences")
+ax.set_ylabel("Shapelet extraction\nTime (s)")
+ax.set_xscale("log")
+fig.tight_layout()
+plt.savefig("imgs/extraction_time.png", dpi=300)
 
-print("="*60)
-print("Many times the clustering has a better accuracy, but when the")
-print("random selection works better, then it does so with fewer candidates")
-db.sql("""
-    WITH
-        base AS (
-            SELECT dataset, k, extraction, n_candidates, accuracy 
-            FROM classification 
-                NATURAL JOIN num_candidates 
-                NATURAL JOIN valid_datasets
-            WHERE evaluation = 'silhouette' 
-              AND extraction != 7 -- exclude FSS
-        ),
-        pivot_table AS (
-            PIVOT base
-            ON extraction IN (6, 8)
-            USING avg(n_candidates) AS n_candidates, avg(accuracy) AS accuracy
-            GROUP BY k, dataset
-        ),
-        situations AS (
-            SELECT k, CASE
-                WHEN "6_accuracy" < "8_accuracy" THEN 'clustering_better'
-                WHEN "6_n_candidates" < "8_n_candidates" THEN 'random_better_fewer_candidates'
-                WHEN "6_n_candidates" >= "8_n_candidates" THEN 'random_better_more_candidates'
-            END AS situation
-            FROM pivot_table
-        )
-    SELECT k, situation, count(*) 
-    FROM situations
-    GROUP BY ALL
-    ORDER BY ALL
-""").show()
 
-print("These are the results to be plotted")
-res_classification = db.sql("""
-    SELECT k, extraction, n_candidates, accuracy 
-    FROM classification 
-        NATURAL JOIN num_candidates 
-        NATURAL JOIN valid_datasets
-    WHERE evaluation = 'silhouette' 
-    """)
-res_classification.show()
-df = res_classification.to_df()
+# Number of candidates
+fig, ax = plt.subplots(figsize=(14 * cm, 6 * cm))
+sns.scatterplot(
+    pd.read_parquet('results/data/numcandidates.parquet.gzip'),
+    x="numcandidates",
+    y='Number of candidates',
+    hue='extraction method',
+    palette=extraction_colors,
+)
+ax.set_yscale("log")
+ax.set_xscale("log")
+ax.set_xlabel("Number of possible candidates")
+ax.set_ylabel("Number of candidates\n(log scale)")
+ax.legend(loc="lower center", fancybox=True, framealpha=0.9, ncol=2)
+fig.tight_layout()
+plt.savefig("imgs/clustering_fss_extraction.png", dpi=300)
 
-if not os.path.isdir("imgs"):
-    os.mkdir("imgs")
 
-fig, ax = plt.subplots(figsize=(14 * cm, 5 * cm))
+# Distance evaluation
+fig, ax = plt.subplots(figsize=(14 * cm, 7 * cm))
+sns.scatterplot(
+    pd.read_parquet('results/data/distances.parquet.gzip'),
+    x="numcandidates",
+    y="time",
+    hue="extraction",
+    ax=ax,
+    palette=extraction_colors
+)
+ax.set_xlabel("Number of possible subsequences")
+ax.set_ylabel("Distance evaluation\nTime (s)")
+ax.set_yscale("log")
+ax.set_xscale("log")
+fig.tight_layout()
+plt.savefig("imgs/distance_time.png", dpi=300)
+
+
+# Accuracy plots
+accuracy_df = pd.read_parquet('results/data/accuracy.parquet.gzip')
+
+## FSS vs Clustering
+fig, ax = plt.subplots(figsize=(14 * cm, 6 * cm))
 sns.boxplot(
-    df.replace(remaps),
+    accuracy_df[(accuracy_df.evaluation == "silhouette")],
     x="k",
     y="accuracy",
     hue="extraction",
@@ -98,24 +86,39 @@ sns.boxplot(
 ax.legend(loc="lower center", fancybox=True, framealpha=0.9, ncol=3)
 ax.set(ylim=(0.36, 1.01))
 fig.tight_layout()
-plt.savefig("imgs/matteo_extraction_comparison.png")
+plt.savefig("imgs/extraction_comparison.png", dpi=300)
 
-# fig, ax = plt.subplots(figsize=(14 * cm, 5 * cm))
-g = sns.FacetGrid(
-    df.replace(remaps),
-    col="k",
-    col_wrap=3
-)
-g.map_dataframe(
-    sns.scatterplot,
-    x="n_candidates",
+## Evaluation methods
+fig, axs = plt.subplots(2, 1, figsize=(14 * cm, 8 * cm), sharex=True)
+ax = axs[0]
+sns.boxplot(
+    accuracy_df[(accuracy_df.extraction == 'clustering')],
+    x="k",
     y="accuracy",
-    hue="extraction",
-    palette=extraction_colors,
-    hue_order=extraction_order,
+    hue="evaluation",
+    showfliers=False,
+    palette=evaluation_colors,
+    hue_order=evaluation_order,
+    ax=ax
 )
-g.add_legend()
-plt.tight_layout()
-plt.savefig("imgs/matteo_extraction_comparison_scatter.png")
+ax.get_legend().remove()
+ax.set(ylim=(0.36, 1.01))
+ax.set_xlabel(None);
+ax.set_ylabel("clustering");
 
-
+ax = axs[1]
+sns.boxplot(
+    accuracy_df[(accuracy_df.extraction == 'FSS')],
+    x="k",
+    y="accuracy",
+    hue="evaluation",
+    showfliers=False,
+    palette=evaluation_colors,
+    hue_order=evaluation_order,
+    ax=ax
+)
+ax.legend(loc="lower right", fancybox=True, framealpha=0.9, ncol=3)
+ax.set(ylim=(0.36, 1.01))
+ax.set_ylabel("FSS");
+fig.tight_layout()
+plt.savefig("imgs/evaluation_comparison.png", dpi=300)
