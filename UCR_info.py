f5cea5e4f47bc923fb1515694476af97c97d3c65
Adam Charane <adamcharane@gmail.com>
2024-10-04 17:29:08 +0200

    Create sqlite database

--- UCR_info.py
+++ UCR_info.py
@@ -1,11 +1,12 @@
+import sqlite3
 import pandas as pd
 
 INFO_URL = "https://www.cs.ucr.edu/~eamonn/time_series_data_2018/DataSummary.csv"
-INFO_PATH = "resources/datasets_info.parquet"
+DB = sqlite3.connect("resources/results.db")
 df = pd.read_csv(INFO_URL).rename(columns=lambda x: x.strip())
-__cast_columns = {'ID': 'int16', 'Length': 'int16',
-                  'Train': 'int16', 'Test': 'int16', 'Class': 'int16'}
+__cast_columns = {'ID': 'int', 'Length': 'int',
+                  'Train': 'int', 'Test': 'int', 'Class': 'int'}
 __same_length_mask = df.Length != "Vary"
 __selected_columns = ['ID', 'Name', 'Length', 'Train', 'Test', 'Class']
 df = df[__same_length_mask].astype(__cast_columns)[__selected_columns]
-df.to_parquet(path=INFO_PATH, index=False, compression='gzip')
+df.to_sql(name="ucr_info", con=DB, index=False, if_exists="replace")
